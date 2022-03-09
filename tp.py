066af30b10c2595193e6c837dedf87234d022d43
Andreas <and@reas.me>
2022-03-09 12:56:23 -0500

    add debugging launch.json file and refine template

--- tp.py
+++ tp.py
@@ -3,12 +3,14 @@
 debug = False
 # debug = True
 
+
 def dprint(*args, **kwargs):
     if not debug:
         return
-    print( "debug:\t"+" ".join(map(str,args)), **kwargs )
+    print("debug:\t"+" ".join(map(str, args)), **kwargs)
+
 
 ttt = int(input())
 
 for _ in range(ttt):
-    map(int,input().split())
+    map(int, input().split())
